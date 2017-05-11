20aa1639223aef4d5cc62cecad79634a60121d7a
samtimberlan <timudoma@gmail.com>
2017-05-11 12:04:14 +0100

    Update WordsChecker.js
    refractor code to fix conflict

--- WordsChecker.js
+++ WordsChecker.js
@@ -1,99 +1,22 @@
+const wordChecker = (wordsPassed) => {
 
-
-class Car {
-  constructor(name, model, type, numOfDoors, isSaloon, speed, numOfWheels){
-	this.name = name;
-	this.model = model;
-	this.type = type;
-	this.numOfDoors = 4;
-	this.isSaloon = true;
-	this.speed = "";
-	numOfWheels = "4";
-  
-
-	if(name === null || model === null || type === null || name === undefined || model === undefined || type === undefined || name === "" || model === "" || type === "")
-	{
-	  this.type = "General";
-	  this.model = "GM";
-	  this.name = "Vehicle";
-	}
-	
-	if(name === "porsche" || name ==="koenigsegg" || name === "Porsche" || name ==="Koenigsegg")
-	{
-	  this.numOfDoors = 2;
-	  
-	}
-	
-	if(type === "trailer")
-	{
-	  this.numOfWheels = 8;
-	  this.isSaloon = false;
-	  
-	}
-  }
-	
-
-
-drive(gear)
-{ 
-  if(gear === 1)
-  {
-    if(this.name === "porsche" || this.name ==="Porsche")
-    {
-      this.speed = "60km/h";
-      console.log("Driving somebody at " + this.speed + " speed.");
+    if(typeof(wordsPassed) === 'string'){
+      let result = {};
+      let wordHolder = wordsPassed.toLowerCase().split(' ');
+        for(counter = 0; counter < wordHolder.length; counter++){
+          console.log(wordHolder[counter])
+            if(wordHolder[counter] in result){
+              let valr = result[wordHolder[[counter]]];
+              result[wordHolder[counter]] = valr+1;
+            }
+            else{
+              result[wordHolder[counter]] = 1;
+            }
     }
-    else
-    {
-      this.speed = "20km/h";
-      console.log("Driving somebody at " + this.speed + " speed.");
+     return JSON.stringify(result);
     }
-  }
-  
-  else if(gear === 2)
-  {
-    if(this.name === "porsche" || this.name ==="Porsche")
-    {
-      this.speed = "30km/h";
+    else{
+      console.log("Please enter an array");
     }
-    this.speed = "50km/h";
-  }
-  
-  else if(gear === 3)
-  {
-    if(this.name === "porsche" || this.name ==="Porsche")
-    {
-      this.speed = "100km/h";
-    }
-    this.speed = "70km/h";
-  }
-  
-  else if(gear === 4)
-  {
-    if(this.name === "porsche" || this.name ==="Porsche")
-    {
-      this.speed = "150km/h";
-    }
-    this.speed = "90km/h";
-  }
-  
-  else if(gear === 5)
-  {
-    if(this.name === "porsche" || this.name ==="Porsche")
-    {
-      this.speed = "250km/h";
-    }
-    this.speed = "120km/h";
-  }
-
-  else if(gear === 7 && type === 'trailer'){
-    this.speed = "77km/h";
-  }
-  
-  else 
-  {
-    console.log("In order to drive, gear must be between 1 and 5");
-  }
 }
-}
-module.exports = new Car
+wordChecker('car : carpet as java : javascript!!&@$%^&');
